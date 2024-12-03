91bed7c839960094b93d0a5052ce77510580fec9
RamedicasPredicciones <laura.aguirre@ramedicas.com>
2024-12-03 10:08:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -82,19 +82,27 @@ if codigo:
         # Guardar la selección y datos en el archivo Excel
         if st.button('Guardar consulta'):
             selected_data = search_results[search_results['numlote'] == nuevo_lote]
-            selected_data = selected_data[['codart', 'numlote', 'cantidad', 'cod_barras', 'nomart', 'presentacion', 'fechavencelote']].copy()
-            selected_data['cantidad'] = cantidad
+            
+            # Asegurarse de que las columnas necesarias estén presentes
+            required_columns = ['codart', 'numlote', 'cantidad', 'cod_barras', 'nomart', 'presentacion', 'fechavencelote']
+            missing_columns = [col for col in required_columns if col not in selected_data.columns]
+            if missing_columns:
+                st.error(f"Faltan las siguientes columnas: {', '.join(missing_columns)}")
+            else:
+                # Si las columnas están presentes, procesar los datos
+                selected_data = selected_data[required_columns].copy()
+                selected_data['cantidad'] = cantidad
 
-            # Crear archivo Excel en memoria
-            consultas_excel = convertir_a_excel(selected_data)
+                # Crear archivo Excel en memoria
+                consultas_excel = convertir_a_excel(selected_data)
 
-            # Proveer opción de descarga
-            st.success("Consulta guardada con éxito!")
-            st.download_button(
-                label="Descargar Excel con la consulta guardada",
-                data=consultas_excel,
-                file_name='consulta_guardada.xlsx',
-                mime="application/vnd.ms-excel"
-            )
+                # Proveer opción de descarga
+                st.success("Consulta guardada con éxito!")
+                st.download_button(
+                    label="Descargar Excel con la consulta guardada",
+                    data=consultas_excel,
+                    file_name='consulta_guardada.xlsx',
+                    mime="application/vnd.ms-excel"
+                )
     else:
         st.error("Código de artículo no encontrado en el inventario.")
